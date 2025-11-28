911817eb22c2681b23fb367885b0a057c6d10b2d
dannycreations <44817214+dannycreations@users.noreply.github.com>
2025-11-28 10:57:39 +0700

    feat(object): add .length property tests for typed arrays and tuples
    - Update array length test to use typed array declaration.
    - Add test for tuple length.

--- crates/krama_std/tests/props_test.rs
+++ crates/krama_std/tests/props_test.rs
@@ -2,13 +2,19 @@ use krama_core::object::Object;
 use krama_internal::test_eval;
 
 test_eval!(
+  should_get_string_length,
+  "\"hello\".length",
+  Object::Integer(5)
+);
+
+test_eval!(
   should_get_array_length,
-  "[1, 2, 3].length",
+  "const a: i64[] = [1, 2, 3]; a.length",
   Object::Integer(3)
 );
 
 test_eval!(
-  should_get_string_length,
-  "\"hello\".length",
-  Object::Integer(5)
+  should_get_tuple_length,
+  "[1, \"hello\", 3].length",
+  Object::Integer(3)
 );
