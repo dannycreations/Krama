8707f0a3fef7bcf25ac8e7b1eeee0dbf356106d0
dannycreations <44817214+dannycreations@users.noreply.github.com>
2025-12-21 11:53:48 +0700

    feat(globals): add eprint and flush print output
    - Introduce `eprint` global function for writing to standard error.
    - Ensure `print` global function flushes standard output immediately.

--- crates/krama_std/src/globals/print.rs
+++ crates/krama_std/src/globals/print.rs
@@ -25,6 +25,40 @@ pub async fn print<'ast>(
     .write_all(b"\n")
     .await
     .map_err(|e| ErrorKind::RuntimeError(e.to_string()))?;
+  stdout
+    .flush()
+    .await
+    .map_err(|e| ErrorKind::RuntimeError(e.to_string()))?;
+
+  Ok(Object::Void)
+}
+
+#[register_global("eprint")]
+pub async fn eprint<'ast>(
+  _: &'ast Bump,
+  objects: &'ast [Object<'ast>],
+) -> Result<Object<'ast>, ErrorKind> {
+  let mut stderr = io::stderr();
+  for (i, obj) in objects.iter().enumerate() {
+    if i > 0 {
+      stderr
+        .write_all(b" ")
+        .await
+        .map_err(|e| ErrorKind::RuntimeError(e.to_string()))?;
+    }
+    stderr
+      .write_all(obj.to_string().as_bytes())
+      .await
+      .map_err(|e| ErrorKind::RuntimeError(e.to_string()))?;
+  }
+  stderr
+    .write_all(b"\n")
+    .await
+    .map_err(|e| ErrorKind::RuntimeError(e.to_string()))?;
+  stderr
+    .flush()
+    .await
+    .map_err(|e| ErrorKind::RuntimeError(e.to_string()))?;
 
   Ok(Object::Void)
 }
